ffa504160191cee1f0523d6a79092915760d3591
ac-arifuzzaman <ac.arifuzzaman@gmail.com>
2021-10-11 12:36:51 +0600

    first commit after install firebase and implement it

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjGxd6IcTHKwum3cCOpR3qCVFCxAvLif0",
+    authDomain: "ema-jhon-auth-integration.firebaseapp.com",
+    projectId: "ema-jhon-auth-integration",
+    storageBucket: "ema-jhon-auth-integration.appspot.com",
+    messagingSenderId: "937367533476",
+    appId: "1:937367533476:web:db6fc53e111dc92802f331"
+};
+
+export default firebaseConfig;
